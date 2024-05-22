ac0f1844835f568ea02b847d39375c6b6e3dabe0
TON618OFF <rusblackgod@gmail.com>
2024-05-22 23:39:58 +0300

    Добавьте файлы проекта.

---
+++ BASE/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+
+namespace BASE
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static string theme;
+        public static string Theme
+        {
+            get { return theme; }
+            set
+            {
+                theme = value;
+                var dict = new ResourceDictionary { Source = new Uri($"/ThemeChanger;component/Themes/{value}.xaml", UriKind.Relative) };
+
+                Current.Resources.MergedDictionaries.RemoveAt(0);
+                Current.Resources.MergedDictionaries.Insert(0, dict);
+
+                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                File.WriteAllText($"{desktop}\\theme.txt", value);
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (File.Exists($"{desktop}\\theme.txt"))
+            {
+                Theme = File.ReadAllText($"{desktop}\\theme.txt");
+            }
+        }
+    }
+
+}
